862dbfa3f8b85ea52c12c621fd6ca7a10b214d5c
Kayode, Kolade Christopher <65571316+Ksound22@users.noreply.github.com>
2020-06-02 00:09:40 +0100

    Add files via upload

---
+++ day3.js
@@ -0,0 +1,222 @@
+alert("Hello World!")
+
+//Functions
+//Writing functions
+/*function total(one, two) {
+	totalThis = one + two;
+	return totalThis;
+}
+var result = total(2, 3);
+console.log(result); */
+
+//Writing with strings
+/*function sayHello(name, age) {
+	console.log("Hi " + name + age);
+}
+sayHello("Happpines, ", 5 + " years old");
+sayHello("Dominic, ", 2 + " years old");   
+sayHello("Richard, ", 8 + " years old" ); */
+
+
+/*function chelseaBoys(name, shirtnum) {
+	console.log("Whatsup " + name + shirtnum);
+}
+chelseaBoys("Kante " + "number ", 7);
+chelseaBoys("Kepa " + "number ", 1);
+chelseaBoys("Rudiger " + "Hustle master number ", 2); */
+
+//Function reuse
+/*function total (one, two) {
+	return one + two;
+}
+var result = total(4, 4)
+console.log(result); */
+
+//To reuse
+
+/*function total (one, two) {
+	return one + two;
+}
+var result = total(4, 4);
+var anotherOne = total(34, 1);
+var theOther = total(3, 3);    //resuse simply means passing more values
+
+console.log(result);
+console.log(anotherOne); //calling the function multiple times
+console.log(theOther);  */
+
+//validation? Will find out!
+/*function namecheck(name) {
+	if (name == "Kolade") {
+		return "Whatsup Kolade"  //return statements are optional
+	} else {
+		return "This is not Kolade"
+	}
+}
+var result = namecheck("Kolawole");
+console.log(result); */
+
+
+/*function namecheck(name) {
+	if (name == "Dominic") {
+		return "This is my nephew";
+	} else {
+		return "This is not my nephew";
+	}
+}
+var result = namecheck("Dominic")
+console.log(result); */
+
+//writing more in order to know how to write functions. This is the hardest part of JS for me
+/*function total (one, two) {
+	return one + two
+}
+var result = total(1, 2);
+console.log(result);          
+
+function namecheck (name) {
+	if (name == "Happiness") {
+		return "This is my first maternal nephew"
+	} else {
+		return "This is not my nephew o"
+	}
+}
+var result = namecheck("Happiness")
+console.log(result);   */
+
+/*function total (one, two) {
+	return one / two
+}
+var result = total(60, 3);
+console.log(result); */ 	//Lesson: the identifier in line 72 was treated as / instead of +
+
+
+/*function total (one, two) {
+	return one + two;
+}
+var result = total(500, 9000);
+console.log(result); */           //Another lesson: line 91 returned 63 cuz i did not comment it off before console logging here. ES6 will come to the rescue soon
+
+/*function total (one, two) {
+	return one * two;
+}
+var result = total(30, 2);
+console.log(result); */
+
+/*function namecheck(name) {
+	if (name == "Chelsea") {
+		return("This is the best team in London")
+	} else {
+		return("This is not Chelsea")
+	}
+}
+var result = namecheck("Chelsea")
+console.log(result); */
+
+//To be specific with function calling...
+/*function shopping() {
+	if (arguments.length = 4) {
+		console.log("You are getting a discount mate")
+	} else if (arguments.length = 2) {
+		console.log("You are not getting any discount")
+	}
+}
+//calling the function 
+shopping('Team'); */
+
+
+/*function namecheck(name) {
+	if (name == "Ksound") {
+		console.log("My nick is Ksound")
+	} else {
+		console.log("Hell no! That is not my nickname")
+	}
+}   
+var result = namecheck("Ksound");
+console.log(result); */
+
+/*function team() {
+	if (arguments.length = 4 ) {
+		console.log("Chelsea is the most successful London club in the last ten years")
+	} else if(arguments.length = 2) {
+		console.log("Arsenal is the most successful club in London")
+	}
+}
+//calling the function
+team("team"); */
+  
+
+
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
